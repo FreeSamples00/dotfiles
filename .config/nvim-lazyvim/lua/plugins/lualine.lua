4dcbe1d19d6250b94dbad2e2a6bb9896ca89b7b0
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-07-24 16:10:58 -0500

    added tmux indicator to nvim

--- .config/nvim-lazyvim/lua/plugins/lualine.lua
+++ .config/nvim-lazyvim/lua/plugins/lualine.lua
@@ -8,6 +8,22 @@ return {
             return os.date("%-I:%M %p")
           end,
         },
+        lualine_x = {
+          {
+            function()
+              if os.getenv("TMUX") then
+                return ""
+              else
+                return ""
+              end
+            end,
+            color = {
+              fg = "#7AA87F",
+              bg = nil,
+              gui = "bold",
+            },
+          },
+        },
       },
     },
   },
