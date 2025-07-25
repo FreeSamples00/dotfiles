34765e6291168533f5c74f44913b5248eb5176c9
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-07-25 12:22:52 -0500

    fix previous TMUX addition (no longer overwrites defaults)

--- .config/nvim-lazyvim/lua/plugins/lualine.lua
+++ .config/nvim-lazyvim/lua/plugins/lualine.lua
@@ -1,30 +1,29 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = {
-      sections = {
-        lualine_z = {
-          function()
-            return os.date("%-I:%M %p")
-          end,
+    opts = function(_, opts)
+      opts.sections.lualine_z = {
+        function()
+          return os.date("%-I:%M %p")
+        end,
+      }
+
+      opts.sections.lualine_x = opts.sections.lualine_x or {}
+      table.insert(opts.sections.lualine_x, 1, {
+        function()
+          if os.getenv("TMUX") then
+            return ""
+          else
+            return ""
+          end
+        end,
+        color = {
+          fg = "#7AA87F",
+          bg = nil,
+          gui = "bold",
         },
-        lualine_x = {
-          {
-            function()
-              if os.getenv("TMUX") then
-                return ""
-              else
-                return ""
-              end
-            end,
-            color = {
-              fg = "#7AA87F",
-              bg = nil,
-              gui = "bold",
-            },
-          },
-        },
-      },
-    },
+      })
+      return opts
+    end,
   },
 }
