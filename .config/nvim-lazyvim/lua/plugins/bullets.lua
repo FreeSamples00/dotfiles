407fe6b0e86930b9264d449d3c63ecb4a5a217e6
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-08-05 16:23:29 -0500

    redid bullets.nvim

--- .config/nvim-lazyvim/lua/plugins/bullets.lua
+++ .config/nvim-lazyvim/lua/plugins/bullets.lua
@@ -1,12 +1,14 @@
 return {
   "bullets-vim/bullets.vim",
+
   config = function()
     vim.g.bullets_delete_last_bullet_if_empty = 2
+    vim.g.bullets_nested_checkboxes = 0
+    vim.g.bullets_checkbox_markers = " x"
+    vim.g.bullets_outline_levels = { "num", "abc", "std-" }
 
-    vim.keymap.set("v", "<leader>m#", "<cmd>RenumberSelection<CR><Esc>", { desc = "Renumber lists in selection" })
-    vim.keymap.set("n", "<leader>mx", "<cmd>ToggleCheckbox<cr>", { desc = "Toggle checkbox" })
-    vim.keymap.set("i", "<C-,>", "<cmd>BulletPromote<cr>", { desc = "Promote bullet" })
-    vim.keymap.set("i", "<C-.>", "<cmd>BulletDemote<cr>", { desc = "Demote Bullet" })
+    vim.keymap.set("i", "<C-h>", "<cmd>BulletPromote<cr>", { desc = "Promote bullet" })
+    vim.keymap.set("i", "<C-l>", "<cmd>BulletDemote<cr>", { desc = "Demote Bullet" })
     vim.keymap.set({ "i", "n" }, "<C-x>", "<cmd>ToggleCheckbox<cr>", { desc = "Toggle Checkbox" })
   end,
 }
