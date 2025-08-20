d0bf7b6fcf74ffcaac16df99b1df9bc36c0fda1e
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-08-20 14:35:06 -0500

    added ctrl [ and ] for bullet point shifting

--- .config/nvim-lazyvim/lua/plugins/bullets.lua
+++ .config/nvim-lazyvim/lua/plugins/bullets.lua
@@ -2,13 +2,15 @@ return {
   "bullets-vim/bullets.vim",
 
   config = function()
+    -- Settings
     vim.g.bullets_delete_last_bullet_if_empty = 2
     vim.g.bullets_nested_checkboxes = 0
     vim.g.bullets_checkbox_markers = " x"
     vim.g.bullets_outline_levels = { "num", "abc", "std-" }
 
-    vim.keymap.set("i", "<C-h>", "<cmd>BulletPromote<cr>", { desc = "Promote bullet" })
-    vim.keymap.set("i", "<C-l>", "<cmd>BulletDemote<cr>", { desc = "Demote Bullet" })
+    -- Keybinds
+    vim.keymap.set({ "i", "n" }, "<C-[>", "<cmd>BulletPromote<cr>", { desc = "Promote bullet" })
+    vim.keymap.set({ "i", "n" }, "<C-]>", "<cmd>BulletDemote<cr>", { desc = "Demote Bullet" })
     vim.keymap.set({ "i", "n" }, "<C-x>", "<cmd>ToggleCheckbox<cr>", { desc = "Toggle Checkbox" })
   end,
 }
