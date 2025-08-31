6b61ad3d150e0278f60956c7d80f0131589c5582
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-08-31 18:40:44 -0500

    added consistent tabbing

--- .config/nvim-lazyvim/lua/config/options.lua
+++ .config/nvim-lazyvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+--- Set tab and indent settings
+vim.opt.tabstop = 2 -- A tab character will display as 2 spaces
+vim.opt.shiftwidth = 2 -- Auto-indentation and << >> operations use 2 spaces
+vim.opt.expandtab = true -- Insert spaces instead of tab characters
+vim.opt.softtabstop = 2 -- When typing, a tab character (or the equivalent spaces) will be 2 spaces wide. Useful with expandtab.- Add any additional options here
