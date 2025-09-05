de0bd765ae9014df2da4db52e5cd5baf16cf16d0
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-05 17:37:41 -0500

    added am/pm

--- .config/sketchybar/scripts/clock.sh
+++ .config/sketchybar/scripts/clock.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# time formatting seems to follow C rules: https://man7.org/linux/man-pages/man3/strftime.3.html
-sketchybar --set "$NAME" label="$(date '+%b%e %l:%M')"
+# formatting follows https://en.wikipedia.org/wiki/ISO_8601
+sketchybar --set "$NAME" label="$(date '+%b%e %l:%M') $(date "+%p" | gsed 's/\(AM\|PM\)/\L&/g')"
