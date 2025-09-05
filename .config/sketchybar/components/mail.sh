5c5e5d877cedc7c3bba82bf288ee3c5e96200b8d
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-05 17:07:13 -0500

    refactored a lil

--- .config/sketchybar/components/mail.sh
+++ .config/sketchybar/components/mail.sh
@@ -2,11 +2,12 @@
 
 SIDE="$1"
 NAME="mail.$SIDE"
+REFRESH_INTERVAL=120
 
 sketchybar \
   --add item "$NAME" "$SIDE" \
   --set "$NAME" \
-        update_freq=120 \
+        update_freq="$REFRESH_INTERVAL" \
         script="$SCRIPT_DIR/mail.sh" \
         click_script="open -a mail"  \
         icon="󰶈" \
