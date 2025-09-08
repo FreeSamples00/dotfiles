b13eb7313d739d32ea13658feec32182ddeebde7
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-07 21:44:45 -0500

    finished am/pm on clock

--- .config/sketchybar/scripts/clock.sh
+++ .config/sketchybar/scripts/clock.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 # formatting follows https://en.wikipedia.org/wiki/ISO_8601
-sketchybar --set "$NAME" label="$(date '+%b%e %l:%M') $(date "+%p" | gsed 's/\(AM\|PM\)/\L&/g')"
+
+case $(date "+%p") in
+  AM)
+    AM_PM=" am"
+    ;;
+  PM)
+    AM_PM=" pm"
+    ;;
+  *)
+    AM_PM=""
+    ;;
+esac
+
+sketchybar --set "$NAME" label="$(date '+%b%e %l:%M')$AM_PM"
