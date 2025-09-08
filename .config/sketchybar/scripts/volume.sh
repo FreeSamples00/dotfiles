ad6f7a643fb3025f3050be9ab6c16216fe918d15
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-07 21:44:57 -0500

    changed volume symbols

--- .config/sketchybar/scripts/volume.sh
+++ .config/sketchybar/scripts/volume.sh
@@ -3,17 +3,27 @@
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
 
+HIGH=""
+MEDIUM=""
+LOW=""
+MUTE=""
+
+# HIGH="󰕾"
+# MEDIUM="󰖀"
+# LOW="󰕿"
+# MUTE="󰖁"
+
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
 
   case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
+    [6-9][0-9]|100) ICON="$HIGH"
     ;;
-    [3-5][0-9]) ICON="󰖀"
+    [3-5][0-9]) ICON="$MEDIUM"
     ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
+    [1-9]|[1-2][0-9]) ICON="$LOW"
     ;;
-    *) ICON="󰖁"
+    *) ICON="$MUTE"
   esac
 
   sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
