8bce434872feeb1282bf32778147928633a81c05
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-14 21:26:34 -0500

    added indicator for empty workspaces

--- sketchybar.pkg/dot-config/sketchybar/scripts/aerospace.sh
+++ sketchybar.pkg/dot-config/sketchybar/scripts/aerospace.sh
@@ -1,10 +1,19 @@
-#! /bin/bash
+#!/bin/bash
+
+source "$CONFIG_DIR/aesthetics.sh"
 
 if [ "$1" == "$FOCUSED_WORKSPACE" ]; then
     sketchybar \
-      --set "$NAME" label.highlight=on
+      --set "$NAME" label.color="$WORKSPACE_FOCUSED_COLOR"
 else
+
+  if [ "$(aerospace list-windows --workspace "$1")" == "" ]; then
     sketchybar \
-      --set "$NAME" label.highlight=off
+      --set "$NAME" label.color="$WORKSPACE_EMPTY_COLOR"
+  else
+    sketchybar \
+      --set "$NAME" label.color="$WORKSPACE_DEFAULT_COLOR"
+  fi
+
 fi
 
