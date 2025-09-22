3985108abf01f67070fce8c728fbea71d67e3ce1
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-22 12:24:14 -0500

    added dropdown that shows time remaining on battery

--- sketchybar.pkg/dot-config/sketchybar/components/battery.sh
+++ sketchybar.pkg/dot-config/sketchybar/components/battery.sh
@@ -1,8 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+
+# Component
 
 SIDE="$1"
 NAME="battery.$SIDE"
 REFRESH_INTERVAL=120
+
+POPUP_NAME="battery.remaining"
+POPUP_REFRESH_INTERVAL=20
 
 sketchybar  \
   --add item "$NAME" "$SIDE" \
@@ -10,4 +15,15 @@ sketchybar  \
   --set "$NAME" \
         update_freq="$REFRESH_INTERVAL" \
         script="$SCRIPT_DIR/battery.sh" \
-        click_script="open 'x-apple.systempreferences:com.apple.Battery-Settings.extension'"
+        click_script="sketchybar -m --set \$NAME popup.drawing=toggle" \
+        popup.align=center \
+
+sketchybar \
+  --add item "$POPUP_NAME" "popup.$NAME" \
+  --set "$POPUP_NAME" \
+        label="--:--" \
+        icon="" \
+        topmost=on \
+        update_freq=$POPUP_REFRESH_INTERVAL \
+        script="$SCRIPT_DIR/battery_remaining.sh" \
+        click_script="$SCRIPT_DIR/battery_remaining.sh"
---
+++ sketchybar.pkg/dot-config/sketchybar/scripts/battery_remaining.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# script
+
+sketchybar \
+  --set "$NAME" \
+    label="--:--"
+
+TIME_REMAINING=$(pmset -g batt + grep 'remaining' | awk -F';' '{print $3}' | awk -F' ' '{print $1}' | xargs)
+
+if ( echo "$TIME_REMAINING" | grep "^\d\d:\d\d$" >/dev/null ); then
+  sketchybar \
+    --set "$NAME" \
+          label="$TIME_REMAINING"
+fi
