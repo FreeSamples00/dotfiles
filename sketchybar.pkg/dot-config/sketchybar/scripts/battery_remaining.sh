19e7195597b3e711f3859c8c4bfff2607bcff970
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-09-22 22:34:38 -0500

    fixed it forreal

--- sketchybar.pkg/dot-config/sketchybar/scripts/battery_remaining.sh
+++ sketchybar.pkg/dot-config/sketchybar/scripts/battery_remaining.sh
@@ -8,7 +8,7 @@ sketchybar \
 
 TIME_REMAINING=$(pmset -g batt + grep 'remaining' | awk -F';' '{print $3}' | awk -F' ' '{print $1}' | xargs)
 
-if ( echo "$TIME_REMAINING" | grep "^\d:\d\d$" >/dev/null ); then
+if ( echo "$TIME_REMAINING" | grep "^.*:.*$" >/dev/null ); then
   sketchybar \
     --set "$NAME" \
           label="$TIME_REMAINING"
