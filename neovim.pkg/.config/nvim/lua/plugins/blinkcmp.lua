0cad818993786b977ad80313dd7c9ad8f15000b9
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-10-29 19:05:58 -0500

    added tab support for completions

--- neovim.pkg/.config/nvim/lua/plugins/blinkcmp.lua
+++ neovim.pkg/.config/nvim/lua/plugins/blinkcmp.lua
@@ -4,10 +4,23 @@ return {
   "saghen/blink.cmp",
 
   opts = {
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
     completion = {
       list = {
-
         -- manually select items from list, text only inserted on acceptance
+        selection = { preselect = false, auto_insert = false },
+      },
+    },
+    cmdline = {
+      keymap = { preset = "inherit" },
+      completion = {
+        menu = { auto_show = true },
+      },
+      list = {
         selection = { preselect = false, auto_insert = false },
       },
     },
