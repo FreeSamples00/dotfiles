85e87b95757188c615074d311c61078c8a73f3ea
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-11-01 17:02:55 -0500

    added autocomplete mapping

--- neovim.pkg/.config/nvim/lua/plugins/blinkcmp.lua
+++ neovim.pkg/.config/nvim/lua/plugins/blinkcmp.lua
@@ -8,18 +8,9 @@ return {
       preset = "default",
       ["<Tab>"] = { "select_next", "fallback" },
       ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<Enter>"] = { "accept", "fallback" },
     },
     completion = {
-      list = {
-        -- manually select items from list, text only inserted on acceptance
-        selection = { preselect = false, auto_insert = false },
-      },
-    },
-    cmdline = {
-      keymap = { preset = "inherit" },
-      completion = {
-        menu = { auto_show = true },
-      },
       list = {
         selection = { preselect = false, auto_insert = false },
       },
