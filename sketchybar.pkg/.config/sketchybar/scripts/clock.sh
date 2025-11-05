48ada9d056a5d6c06e184369351f66574bb72c78
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-11-04 23:29:45 -0600

    there is a format specifier for lowercase am/pm 🤦

--- sketchybar.pkg/.config/sketchybar/scripts/clock.sh
+++ sketchybar.pkg/.config/sketchybar/scripts/clock.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 # script
 
-case $(gdate "+%p") in
-  AM)
-    AM_PM=" am"
-    ;;
-  PM)
-    AM_PM=" pm"
-    ;;
-  *)
-    AM_PM=""
-    ;;
-esac
-
 # use - in format specifiers to remove padding
-sketchybar --set "$NAME" label="$(gdate '+%b %-e %-l:%M')$AM_PM"
+sketchybar --set "$NAME" label="$(gdate '+%b %-e %-l:%M %P')"
