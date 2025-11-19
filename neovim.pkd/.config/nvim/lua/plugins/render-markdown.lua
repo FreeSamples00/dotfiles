504e4ba852c1ca468c868fee24f2ccaa6d5414d0
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-11-19 13:06:12 -0600

    fixed latex rendering, changed heading symbols

--- neovim.pkd/.config/nvim/lua/plugins/render-markdown.lua
+++ neovim.pkd/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,27 +1,24 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
 
-  latex = {
-    -- Turn on / off latex rendering.
-    enabled = true,
-    -- Additional modes to render latex.
-    render_modes = false,
-    -- Executable used to convert latex formula to rendered unicode.
-    converter = "latex2text",
-    -- Highlight for latex blocks.
-    highlight = "RenderMarkdownMath",
-    -- Determines where latex formula is rendered relative to block.
-    -- | above | above latex block |
-    -- | below | below latex block |
-    position = "above",
-    -- Number of empty lines above latex blocks.
-    top_pad = 0,
-    -- Number of empty lines below latex blocks.
-    bottom_pad = 0,
+  opts = {
+
+    latex = {
+      enabled = true,
+      render_modes = false,
+      converter = { "utftex", "latex2text" },
+      highlight = "RenderMarkdownMath",
+      position = "above",
+      top_pad = 1,
+      bottom_pad = 1,
+    },
+
+    heading = {
+      position = "inline",
+      icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
+    },
   },
 }
