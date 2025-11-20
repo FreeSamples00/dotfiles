49b449a1f2972eb0afbd6d7139b1181722024bdb
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-11-20 15:50:32 -0600

    reorganized and recommented, added ssh indicator

--- neovim.pkd/.config/nvim/lua/plugins/lualine.lua
+++ neovim.pkd/.config/nvim/lua/plugins/lualine.lua
@@ -2,25 +2,26 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function()
+    -- get formatted time
     local function get_time()
       return os.date("%-I:%M %p")
     end
 
+    -- get dirname or root folder of git (cached to avoid flickering cursor)
     local function get_repo_or_dir_name()
       if not vim.g.cached_repo_name or vim.fn.getcwd() ~= vim.g.cached_cwd then
         local git_toplevel = vim.fn.system("git rev-parse --show-toplevel 2>/dev/null"):match("(.*)\n")
-
         if git_toplevel and git_toplevel ~= "" then
           vim.g.cached_repo_name = "󰊢 " .. vim.fn.fnamemodify(git_toplevel, ":t")
         else
-          vim.g.cached_repo_name = " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+          vim.g.cached_repo_name = " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
         end
         vim.g.cached_cwd = vim.fn.getcwd()
       end
-
       return vim.g.cached_repo_name
     end
 
+    -- indicator for tmux session
     local function tmux_indicator()
       if os.getenv("TMUX") then
         return ""
@@ -29,6 +30,7 @@ return {
       end
     end
 
+    -- indicator for macro being recorded
     local function macro_indicator()
       local reg = vim.fn.reg_recording()
       if reg == "" then
@@ -37,38 +39,60 @@ return {
       return " " .. reg
     end
 
+    -- indicator for ssh connection
+    local function ssh_indicator()
+      if vim.env.SSH_CONNECTION or vim.env.SSH_CLIENT or vim.env.SSH_TTY then
+        return "󰒋"
+      end
+      return ""
+    end
+
+    -- actual line settings
     return {
+
+      -- general settings
       options = {
         icons_enabled = true,
         theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        component_separators = {
+          left = "",
+          right = "",
+        },
+        section_separators = {
+          left = "",
+          right = "",
+        },
         disabled_filetypes = {
           "NvimTree",
           "packer",
         },
         always_last_session = true,
-        lsp_progress = {
-          enabled = true,
-        },
+        lsp_progress = { enabled = true },
       },
 
-      -- Define the base sections
+      -- Sections
       sections = {
+        -- Left side | left -> right
         lualine_a = { "mode" },
         lualine_b = { get_repo_or_dir_name },
         lualine_c = {
-          { "branch", icon = "" },
           {
-            "diagnostics",
-            sources = { "nvim_lsp" },
-            symbols = { error = " ", warn = " ", info = " ", hint = " " },
-            colored = true,
-            always_visible = false,
+            "branch",
+            icon = "",
           },
-          "lsp_progress",
+          "diff",
         },
+
+        -- Right Side | left -> right
         lualine_x = {
+          {
+            macro_indicator,
+            color = {
+              fg = "#cb2000",
+              bg = nil,
+              gui = "bold",
+            },
+          },
           {
             tmux_indicator,
             color = {
@@ -78,20 +102,31 @@ return {
             },
           },
           {
-            macro_indicator,
+            ssh_indicator,
             color = {
-              fg = "#cb2000",
+              fg = "#FFD709",
               bg = nil,
               gui = "bold",
             },
           },
-          -- "encoding",
-          "diff",
+          {
+            "diagnostics",
+            sources = { "nvim_lsp" },
+            symbols = {
+              error = " ",
+              warn = " ",
+              info = " ",
+              hint = " ",
+            },
+            colored = true,
+            always_visible = false,
+          },
         },
         lualine_y = { "progress" },
         lualine_z = { get_time },
       },
 
+      -- disabled sections
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
@@ -101,7 +136,7 @@ return {
         lualine_z = {},
       },
 
-      -- Since you're using Tabby for the tabline, ensure lualine's tabline is empty
+      -- disable top things
       tabline = {},
       extensions = {},
     }
