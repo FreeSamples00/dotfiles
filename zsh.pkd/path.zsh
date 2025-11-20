9c54ad624ac4010e86aa0fee9d76bc34e6143936
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-11-20 15:51:18 -0600

    added some python bins to path

--- zsh.pkd/path.zsh
+++ zsh.pkd/path.zsh
@@ -1,4 +1,5 @@
 eval "$(/usr/libexec/path_helper)"
-export PATH="$PATH:/Users/scc/.local/bin"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/Library/Python/3.9/bin"
 
 alias path='print -c ${(s/:/)PATH} | bat --file-name "\$PATH"'
