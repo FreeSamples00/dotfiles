87e17e457cc3ff1840ae44d03fd6537aeb7b8181
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-12-07 00:47:11 -0600

    add homebrew above system

--- zsh.pkd/path.zsh
+++ zsh.pkd/path.zsh
@@ -1,4 +1,8 @@
 eval "$(/usr/libexec/path_helper)"
+
+# Prioritize Homebrew binaries (for FIDO2 support in OpenSSH)
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/Library/Python/3.9/bin"
 
