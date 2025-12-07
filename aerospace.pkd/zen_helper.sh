919f0a21ed9140a6053420625f6d08a83092e438
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-12-07 15:18:10 -0600

    updated aerospace (fixed zen helper??)

--- aerospace.pkd/zen_helper.sh
+++ aerospace.pkd/zen_helper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Script to open zen and ensure it lands on the right workspace
+
 CURRENT_WORKSPACE=$(aerospace list-workspaces --focused)
 open -na Zen
-aerospace move-node-to-workspace "$CURRENT_WORKSPACE" 2&>/dev/null
-aerospace workspace "$CURRENT_WORKSPACE" 2&>/dev/null
+aerospace move-node-to-workspace "$CURRENT_WORKSPACE" &>/dev/null
+aerospace workspace "$CURRENT_WORKSPACE" &>/dev/null
