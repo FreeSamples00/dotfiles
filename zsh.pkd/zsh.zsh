a805e7ab8419d236999d4464fc9890c739edf072
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-12-07 13:27:52 -0600

    massively improved tab complete

--- zsh.pkd/zsh.zsh
+++ zsh.pkd/zsh.zsh
@@ -8,7 +8,14 @@ zinit cdreplay -q
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:*:*' fzf-preview 'bat --color=always --line-range :500 ${(Q)realpath}'
+zstyle ':fzf-tab:*' fzf-flags --color=fg:7,bg:-1,hl:3,fg+:15,bg+:8,hl+:11,info:6,prompt:4,pointer:5,marker:2,spinner:6,header:4
+if (which bat >/dev/null) && (which eza >/dev/null); then
+  zstyle ':fzf-tab:complete:*:*' fzf-preview '[[ -d ${(Q)realpath} ]] && eza --tree --level=1 --sort=modified --reverse --follow-symlinks --dereference --color=always ${(Q)realpath} || bat --color=always --line-range :500 ${(Q)realpath}'
+elif (which bat >/dev/null); then
+  zstyle ':fzf-tab:complete:*:*' fzf-preview '[[ -d ${(Q)realpath} ]] && \ls -AtL --color=always ${(Q)realpath} || bat --color=always --line-range :500 ${(Q)realpath}'
+else
+  zstyle ':fzf-tab:complete:*:*' fzf-preview '[[ -d ${(Q)realpath} ]] && \ls -AtL --color=always ${(Q)realpath} || cat ${(Q)realpath}'
+fi
 
 # ==== HISTORY ====
 
