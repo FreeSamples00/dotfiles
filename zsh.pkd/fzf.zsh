3a379a9b337b732da4a8f22031031e375e96ab1a
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-12-08 19:59:50 -0600

    added default color scheme for fzf

--- zsh.pkd/fzf.zsh
+++ zsh.pkd/fzf.zsh
@@ -3,13 +3,13 @@
 if (( $+commands[fzf] )); then
   eval "$(fzf --zsh)"
   export FZF_DEFAULT_COMMAND='fd --hidden --strip-cwd-prefix --exclude .git'
-  export FZF_DEFAULT_OPTS='--height 50% --layout=default --border --color=hl:#2dd4bf'
+  export FZF_DEFAULT_OPTS='--height 50% --layout=default --border --color=fg:7,bg:-1,hl:3,fg+:15,bg+:8,hl+:11,info:6,prompt:4,pointer:5,marker:2,spinner:6,header:4'
   
   # Disable ctrl+t and alt+c keybindings
   bindkey -r '^T'  # Remove ctrl+t (fzf-file-widget)
   bindkey -r '\ec' # Remove alt+c (fzf-cd-widget)
   
-  # fzf-tab styling
+  # fzf-tab styling - inherit colors from FZF_DEFAULT_OPTS
   zstyle ':fzf-tab:*' fzf-flags --color=fg:7,bg:-1,hl:3,fg+:15,bg+:8,hl+:11,info:6,prompt:4,pointer:5,marker:2,spinner:6,header:4
   if (( $+commands[bat] )) && (( $+commands[eza] )); then
     zstyle ':fzf-tab:complete:*:*' fzf-preview '[[ -d ${(Q)realpath} ]] && eza --tree --level=1 --sort=modified --reverse --follow-symlinks --dereference --color=always ${(Q)realpath} || bat --color=always --line-range :500 -p ${(Q)realpath}'
