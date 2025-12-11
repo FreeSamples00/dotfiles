8a3ab24e417a7932240cc75e6ebc52d80892470c
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-12-11 17:19:29 -0600

    added rc files for file detection

--- neovim.pkd/.config/nvim/ftdetect/dotfiles.lua
+++ neovim.pkd/.config/nvim/ftdetect/dotfiles.lua
@@ -1,9 +1,19 @@
--- Automatically detect filetype for dot-XXXXX files
 -- Maps dot-XXXXX → filetype XXXXX
 vim.filetype.add({
   pattern = {
     ["dot%-(.+)"] = function(_, _, ext)
       return ext
     end,
+  },
+  -- add rc files as their filetype
+  filename = {
+    [".zshrc"] = "zsh",
+    ["dot-zshrc"] = "zsh",
+    [".bashrc"] = "bash",
+    ["dot-bashrc"] = "bash",
+    [".vimrc"] = "vim",
+    ["dot-vimrc"] = "vim",
+    [".screenrc"] = "screen",
+    ["dot-screenrc"] = "screen",
   },
 })
