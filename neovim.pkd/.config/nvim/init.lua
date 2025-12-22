cd6ad9b06564c170234057523ca033d089a89f21
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-12-21 22:51:14 -0600

    cleaned up

--- neovim.pkd/.config/nvim/init.lua
+++ neovim.pkd/.config/nvim/init.lua
@@ -1,18 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
--- -- percentage of screen from top or bottom and which autoscrolling is triggered
--- -- - use 50 for typewriter style
--- local TRIGGER_PERCENTAGE = 35
---
--- local function set_dynamic_scrolloff()
---   local h = vim.api.nvim_win_get_height(0)
---   vim.wo.scrolloff = math.max(1, math.floor(h * (TRIGGER_PERCENTAGE / 100)))
--- end
---
--- local grp = vim.api.nvim_create_augroup("DynamicScrolloff", { clear = true })
--- vim.api.nvim_create_autocmd({ "VimResized", "WinResized", "BufWinEnter", "WinEnter" }, {
---   group = grp,
---   callback = set_dynamic_scrolloff,
--- })
-
 require("config.lazy")
