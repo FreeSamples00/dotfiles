9ed0050525723b134b3187b7ec13ca511150d7f7
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-01-18 01:55:39 -0600

    fixes formatting issue, added error case

--- sketchybar.pkd/.config/sketchybar/executables/clock_window.sh
+++ sketchybar.pkd/.config/sketchybar/executables/clock_window.sh
@@ -1,1 +1,6 @@
-tty-clock -Bbsctn -C 5 -f "%A %m/%d %Y"
+if which tty-clock >/dev/null; then
+  sleep 0.5
+  tty-clock -Bbsctn -C 5 -f "%A %m/%d %Y"
+else
+  echo -e "\n\033[91mERR: tty-clock not installed\033[0m" | less -R
+fi
