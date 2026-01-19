28b1b6753ddd80210a9b086b496f4e2a5e3c400a
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-01-19 15:44:23 -0600

    fixed path

--- sketchybar.pkd/.config/sketchybar/items/volume.sh
+++ sketchybar.pkd/.config/sketchybar/items/volume.sh
@@ -19,6 +19,6 @@ sketchybar --add item volume.label "$SIDE" \
   --set volume.icon \
   icon="$VOLUME_ICON" \
   icon.font="$SBAR_ICON_FONT_FAMILY:Bold:$(calc "$SBAR_ICON_FONT_SIZE + 4")" \
-  click_script="$SBAR_SCRIPT_DIR/toggle_mute.sh" \
+  click_script="$SBAR_EVENT_DIR/volume_toggle.sh" \
   icon.padding_left="$SBAR_ITEM_ICON_PADDING_LEFT" \
   icon.padding_right="$SBAR_ITEM_ICON_PADDING_RIGHT"
