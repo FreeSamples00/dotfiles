21fcd18507ca1277e358144685d29c0b407f34d0
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-03-19 23:04:06 -0500

    cleaner nvim

--- neovim.pkd/.config/nvim/lua/plugins/lualine.lua
+++ neovim.pkd/.config/nvim/lua/plugins/lualine.lua
@@ -8,18 +8,18 @@ return {
     end
 
     -- get dirname or root folder of git (cached to avoid flickering cursor)
-    local function get_repo_or_dir_name()
-      if not vim.g.cached_repo_name or vim.fn.getcwd() ~= vim.g.cached_cwd then
-        local git_toplevel = vim.fn.system("git rev-parse --show-toplevel 2>/dev/null"):match("(.*)\n")
-        if git_toplevel and git_toplevel ~= "" then
-          vim.g.cached_repo_name = "󰊢 " .. vim.fn.fnamemodify(git_toplevel, ":t")
-        else
-          vim.g.cached_repo_name = " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-        end
-        vim.g.cached_cwd = vim.fn.getcwd()
-      end
-      return vim.g.cached_repo_name
-    end
+    -- local function get_repo_or_dir_name()
+    --   if not vim.g.cached_repo_name or vim.fn.getcwd() ~= vim.g.cached_cwd then
+    --     local git_toplevel = vim.fn.system("git rev-parse --show-toplevel 2>/dev/null"):match("(.*)\n")
+    --     if git_toplevel and git_toplevel ~= "" then
+    --       vim.g.cached_repo_name = "󰊢 " .. vim.fn.fnamemodify(git_toplevel, ":t")
+    --     else
+    --       vim.g.cached_repo_name = " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    --     end
+    --     vim.g.cached_cwd = vim.fn.getcwd()
+    --   end
+    --   return vim.g.cached_repo_name
+    -- end
 
     -- indicator for macro being recorded
     local function macro_indicator()
@@ -65,14 +65,22 @@ return {
       sections = {
         -- Left side | left -> right
         lualine_a = { "mode" },
-        lualine_b = { get_repo_or_dir_name },
-        lualine_c = {
+        lualine_b = {
           {
-            "branch",
-            icon = "",
+            "filename",
+            file_status = true,
+            newfile_status = true,
+            path = 0,
+            shorting_target = 20,
+            symbols = {
+              modified = "󰏫",
+              readonly = "󰍁",
+              unnamed = "[Unnamed]",
+              newfile = "",
+            },
           },
-          "diff",
         },
+        lualine_c = { "diff" },
 
         -- Right Side | left -> right
         lualine_x = {
