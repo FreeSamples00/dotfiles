b34ef19b82846ecadfaf712d5fa48297320a6a61
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-04-03 04:11:29 -0500

    buffer exiting

--- neovim.pkd/.config/nvim-new/lua/core/options.lua
+++ neovim.pkd/.config/nvim-new/lua/core/options.lua
@@ -7,15 +7,15 @@ local opts = {
   number = true,
   relativenumber = true,
   clipboard = "unnamedplus", -- use system clipboard
+  undofile = true,
+  confirm = true,
+  autowrite = true,
 }
 
 -- Set options from table
 for opt, val in pairs(opts) do
   vim.o[opt] = val
 end
-
--- enable undofile
-vim.opt.undofile = true
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
