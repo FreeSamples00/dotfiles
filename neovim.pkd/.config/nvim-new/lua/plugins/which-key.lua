ffcd7e586c29a1d623d370a66355f4626c9ec2d7
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-04-05 01:18:07 -0500

    customized which-key

--- neovim.pkd/.config/nvim-new/lua/plugins/which-key.lua
+++ neovim.pkd/.config/nvim-new/lua/plugins/which-key.lua
@@ -4,21 +4,23 @@ return {
     config = function()
       local wk = require("which-key")
       wk.setup({
+        preset = "helix",
+        notify = true,
         keys = {
-          scroll_down = "<c-j>",
-          scroll_up = "<c-k>",
+          scroll_down = "<c-d>",
+          scroll_up = "<c-u>",
         },
       })
       wk.add({
-        -- { "<leader>b", group = "Debugging" },
-        { "<leader>h", group = "Harpoon" },
-        { "<leader>d", group = "Delete/Close" },
-        { "<leader>f", group = "File" },
-        { "<leader>g", group = "Git" },
-        { "<leader>l", group = "LSP" },
-        { "<leader>q", group = "Quit" },
-        { "<leader>s", group = "Search" },
-        { "<leader>u", group = "UI" },
+        { "<leader>h",  group = "Harpoon" },
+        { "<leader>d",  group = "Delete/Close" },
+        { "<leader>f",  group = "File" },
+        { "<leader>g",  group = "Git" },
+        { "<leader>l",  group = "LSP" },
+        { "<leader>q",  group = "Quit" },
+        { "<leader>s",  group = "Search" },
+        { "<leader>u",  group = "UI" },
+        { "<leader>b>", group = "buffer" },
       })
     end,
   },
