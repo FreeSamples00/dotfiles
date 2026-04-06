3882c8dd09c2a9205c083b1d7326bd159ce0d0ee
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-04-05 21:26:24 -0500

    improved options loop, added comments

--- neovim.pkd/.config/nvim/lua/core/options.lua
+++ neovim.pkd/.config/nvim/lua/core/options.lua
@@ -1,21 +1,23 @@
+-- set default options
+
+-- options to apply
 local opts = {
-  shiftwidth = 2,
-  tabstop = 2,
-  softtabstop = 2,
-  expandtab = true,
-  wrap = false,
-  termguicolors = true,
-  number = true,
-  relativenumber = true,
+  shiftwidth = 2,            -- set sane tabbing
+  tabstop = 2,               -- |
+  softtabstop = 2,           -- |
+  expandtab = true,          -- replace tab chars with spaces
+  wrap = false,              -- disable linewrapping by default
+  termguicolors = true,      -- enable 24bit color if supported
+  number = true,             -- enable line numbers
+  relativenumber = true,     -- enable relative line numbers
   clipboard = "unnamedplus", -- use system clipboard
-  undofile = true,
-  confirm = true,
-  autowrite = true,
+  undofile = true,           -- enable undofile to track changes across sessions
+  confirm = true,            -- enable confirm menu on things like saving before quiting
+  scrolloff = 5,             -- keep n lines on screen above and below cursor at all times
+  fillchars = { eob = " " }  -- remove `~` from end of buffer
 }
 
 -- Set options from table
 for opt, val in pairs(opts) do
-  vim.o[opt] = val
+  vim.opt[opt] = val
 end
-
-vim.opt.fillchars = { eob = " " }
