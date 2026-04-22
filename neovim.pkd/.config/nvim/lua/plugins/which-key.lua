36cfa5efb550374c883e20d53d6046779812d976
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2026-04-21 20:56:25 -0500

    nvim: reworked which-key config

--- neovim.pkd/.config/nvim/lua/plugins/which-key.lua
+++ neovim.pkd/.config/nvim/lua/plugins/which-key.lua
@@ -1,28 +1,24 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function()
-      local wk = require("which-key")
-      wk.setup({
-        preset = "helix",
-        notify = true,
-        keys = {
-          scroll_down = "<c-d>",
-          scroll_up = "<c-u>",
-        },
-      })
-      wk.add({
-        { "<leader>h", group = "Harpoon" },
-        { "<leader>d", group = "Delete/Close" },
-        { "<leader>f", group = "File" },
-        { "<leader>g", group = "Git" },
-        { "<leader>l", group = "LSP" },
-        { "<leader>q", group = "Quit" },
-        { "<leader>s", group = "Search" },
-        { "<leader>u", group = "UI" },
-        { "<leader>b", group = "Buffers" },
-      })
-    end,
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "helix",
+    notify = true,
+    keys = { scroll_down = "<c-d>", scroll_up = "<c-u>" },
+    icons = { mappings = false, group = "" },
   },
+  config = function(_, opts)
+    require("which-key").setup(opts)
+    require("which-key").add({
+      { "<leader>h", group = "Harpoon" },
+      { "<leader>d", group = "Delete/Close" },
+      { "<leader>f", group = "File" },
+      { "<leader>g", group = "Git" },
+      { "<leader>l", group = "LSP" },
+      { "<leader>q", group = "Quit" },
+      { "<leader>s", group = "Search" },
+      { "<leader>u", group = "UI" },
+      { "<leader>b", group = "Buffers" },
+    })
+  end,
 }
